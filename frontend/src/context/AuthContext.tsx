5226e3ab31206bfdd9af278a25557a4e1369d7e3
Sedelmaier Stefan <stefan@sedelmaier.at>
2026-03-29 17:14:38 +0200

    Use useMemo to avoid useless render

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -82,7 +82,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     );
 
     return (
-        <AuthContext.Provider value={{value}}>
+       <AuthContext.Provider value={value}>
             {children}
         </AuthContext.Provider>
     );
