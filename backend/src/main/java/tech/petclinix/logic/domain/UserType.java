c272643b7cb82bcbe3677e3056d280364f1b08e0
Sedelmaier Stefan <stefan@sedelmaier.at>
2026-04-04 14:34:50 +0200

    Unused code removed

--- backend/src/main/java/tech/petclinix/logic/domain/UserType.java
+++ backend/src/main/java/tech/petclinix/logic/domain/UserType.java
@@ -15,10 +15,4 @@ public enum UserType {
         return code;
     }
 
-    public static UserType fromCode(String code) {
-        for (UserType s : values()) {
-            if (s.code.equals(code)) return s;
-        }
-        throw new IllegalArgumentException("Unknown Status code " + code);
-    }
 }
