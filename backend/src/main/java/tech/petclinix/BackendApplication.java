3f26851e7b73b9544adf64c2f8956a4dffbfe2ce
Stefan Sedelmaier <stefan@sedelmaier.at>
2026-09-03 15:40:46 +0200

    Add openapi properties

--- backend/src/main/java/tech/petclinix/BackendApplication.java
+++ backend/src/main/java/tech/petclinix/BackendApplication.java
@@ -1,9 +1,16 @@
 package tech.petclinix;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@OpenAPIDefinition(info = @Info(
+		title = "${openapi.info.title}",
+		version = "${openapi.info.version}",
+		description = "${openapi.info.description}"
+))
 @SpringBootApplication
 @EnableAsync
 public class BackendApplication {
